9745a70180e173d23c91c94937b87313c84acf19
Piyush Agarwal <pkdhirasaria@gmail.com>
2018-07-19 17:37:07 +0530

    Update Subset.cpp

--- CODING NINJAS/Subset.cpp
+++ CODING NINJAS/Subset.cpp
@@ -48,3 +48,51 @@ int main()
 
 
 
+
+
+#include <bits/stdc++.h>
+#define fr(a,b) for(i=a;i<b;i++)
+#define rep(a,b) for(i=a;i>=b;i--)
+#define input(a,n) fr(i,n) cin>>a[i]
+#define print(a,n) fr(i,n) {cout << a[i] << " ";} cout << endl
+#define lli long long int
+using namespace std;
+
+void subsetutility(int a[],int n,int i, int output[])
+{
+    if(n == 0)
+    {
+        for(int j =0;j<i;j++)
+        {
+            cout<<output[j]<<" ";
+        }
+        cout<<endl;
+        return;
+    }
+    subsetutility(a+1,n-1,i,output);
+    output[i++] = a[0];
+    subsetutility(a+1,n-1,i,output);
+    return;
+}
+
+
+
+int subset(int a[],int n)
+{
+    int output[100];
+    subsetutility(a,n,0,output);
+    return 1;
+}
+
+
+int main()
+{
+    int t,n;
+    cin>>n;
+    int i = 0;
+    int a[10];
+    input(a,n);
+    int size = subset(a,n);
+}
+
+
